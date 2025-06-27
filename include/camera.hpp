1c0cfbfcc6c4ed648b08d03df5cfbe5ef5094401
Ian Kersz <kerszamaral@gmail.com>
2025-06-27 12:40:58 -0300

    Created camera class
    Co-authored-by: Tomas <tomasmitsuo@gmail.com>

--- include/camera.hpp
+++ include/camera.hpp
@@ -1,1 +1,60 @@
 // TODO: CRIAR UMA CLASSE PARA A CAMERA
+#pragma once
+
+#include <glm/vec4.hpp>
+#include <glm/mat4x4.hpp>
+
+#include "ball.hpp"
+
+class Camera {
+private:
+    glm::vec4 position;
+    glm::vec4 lookAt;
+    glm::vec4 upVector;
+    glm::vec4 projection;
+
+    float theta;
+    float phi;
+    float distance;
+
+    float camera_height;
+
+    
+    float nearPlane;
+    float farPlane;
+    float fieldOfView;
+
+    bool using_perspective_projection;
+
+    const Ball& targetBall;
+
+public:
+    Camera(const Ball &targetBall, float camera_height, float distance, float theta, float phi, float nearPlane, float farPlane, float fieldOfView);
+
+    void updatePosition();
+
+    glm::vec4 getPosition() const;
+    glm::vec4 getLookAt() const;
+    glm::vec4 getUpVector() const;
+    glm::vec4 getViewVector() const;
+
+    glm::mat4 getViewMatrix() const;
+
+    glm::mat4 getProjectionMatrix(float screenRatio) const;
+
+    void setUsingPerspectiveProjection(bool value);
+
+    bool isUsingPerspectiveProjection();
+
+    float getTheta() const;
+    float getPhi() const;
+    float getDistance() const;
+
+    void setTheta(float newTheta);
+    void setPhi(float newPhi);
+    void setDistance(float newDistance);
+
+    void addTheta(float deltaTheta);
+    void addPhi(float deltaPhi);
+    void addDistance(float deltaDistance);
+};
---
+++ src/camera.cpp
@@ -0,0 +1,104 @@
+#include "camera.hpp"
+
+#include <cmath>
+
+#include "matrices.h"
+
+
+Camera::Camera(const Ball &targetBall, float camera_height, float distance, float theta, float phi, float nearPlane, float farPlane, float fieldOfView)
+    : theta(theta), phi(phi), distance(distance),
+        camera_height(camera_height),
+      nearPlane(nearPlane), farPlane(farPlane), fieldOfView(fieldOfView),
+      using_perspective_projection(true), targetBall(targetBall) {
+    updatePosition();
+}
+
+void Camera::updatePosition() {
+    float y = distance * std::sin(phi);
+    float x = targetBall.x + distance * std::sin(theta) * std::cos(phi);
+    float z = targetBall.z + distance * std::cos(theta) * std::cos(phi);
+
+    position = glm::vec4(x, y, z, 1.0f);
+    lookAt = glm::vec4(targetBall.x, camera_height, targetBall.z, 1.0f);
+    upVector = glm::vec4(0.0f, 1.0f, 0.0f, 0.0f);
+}
+
+glm::vec4 Camera::getPosition() const {
+    return position;
+}
+
+glm::vec4 Camera::getLookAt() const {
+    return lookAt;
+}
+
+glm::vec4 Camera::getUpVector() const {
+    return upVector;
+}
+
+glm::vec4 Camera::getViewVector() const {
+    return lookAt - position;
+}
+
+glm::mat4 Camera::getViewMatrix() const {
+    const glm::vec4 view_vector = getViewVector();
+    return Matrix_Camera_View(position, view_vector, upVector);
+}
+
+glm::mat4 Camera::getProjectionMatrix(float screenRatio) const {
+    if (using_perspective_projection)
+    {
+        return Matrix_Perspective(fieldOfView, screenRatio, nearPlane, farPlane);
+    }
+    else
+    {
+        float t = 1.5f * distance / 2.5f;
+        float b = -t;
+        float r = t * screenRatio;
+        float l = -r;
+        return Matrix_Orthographic(l, r, b, t, nearPlane, farPlane);
+    }
+}
+
+void Camera::setUsingPerspectiveProjection(bool value) {
+    using_perspective_projection = value;
+}
+
+bool Camera::isUsingPerspectiveProjection() {
+    return using_perspective_projection;
+}
+
+float Camera::getTheta() const {
+    return theta;
+}
+
+float Camera::getPhi() const {
+    return phi;
+}
+
+float Camera::getDistance() const {
+    return distance;
+}
+
+void Camera::setTheta(float newTheta) {
+    theta = newTheta;
+}
+
+void Camera::setPhi(float newPhi) {
+    phi = newPhi;
+}
+
+void Camera::setDistance(float newDistance) {
+    distance = newDistance;
+}
+
+void Camera::addTheta(float deltaTheta) {
+    theta += deltaTheta;
+}
+
+void Camera::addPhi(float deltaPhi) {
+    phi += deltaPhi;
+}
+
+void Camera::addDistance(float deltaDistance) {
+    distance += deltaDistance;
+}
