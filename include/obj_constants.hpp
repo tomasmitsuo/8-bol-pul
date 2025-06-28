519b03190dae5ced2dc7b104c7d1f9c5137a7b34
kerszamaral <kerszamaral@gmail.com>
2025-06-27 23:44:08 -0300

    Better formatting of object constants
    Co-authored-by: Tomas <tomasmitsuo@gmail.com>

--- include/obj_constants.hpp
+++ include/obj_constants.hpp
@@ -8,19 +8,20 @@ constexpr float TABLE_LENGTH = 5.5f*2;
 constexpr float TABLE_CENTER_X = 0.4f;
 constexpr float TABLE_CENTER_Z = 4.0f;
 
+
 constexpr float BALL_RADIUS = 0.13f;
 constexpr float BALL_DIAMETER = BALL_RADIUS * 2.0f;
 const float ROW_SPACING = std::sqrt(3.0f) * BALL_RADIUS;
+
 
 constexpr float TRIANGULE_CENTER_X = TABLE_CENTER_X;
 constexpr float TRIANGULE_OFFSET_Z = 0.8f;
 constexpr float TRIANGULE_CENTER_Z = TABLE_CENTER_Z - TABLE_LENGTH/3 + TRIANGULE_OFFSET_Z;
 
+
 constexpr float WHITE_BALL_X = TRIANGULE_CENTER_X;
 constexpr float WHITE_BALL_Z = TABLE_CENTER_Z + TABLE_LENGTH/3 - TRIANGULE_OFFSET_Z;
 
-constexpr float MAX_SHOT_STRENGTH = 5.0f;
-constexpr float STRENGTH_INCREMENT = 2.5f;
 
 constexpr float CAMERA_SPEED = 1.0f; // Speed of the camera movement
 constexpr float CAMERA_HEIGHT = TABLE_HEIGHT + 0.1f; // Height of the camera
