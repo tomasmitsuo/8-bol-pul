32fd467bba0870a8c041f2893f53e91e54c933ab
kerszamaral <kerszamaral@gmail.com>
2025-07-02 16:01:03 -0300

    Made bezier animation better
    Co-authored-by: Tomas <tomasmitsuo@gmail.com>

--- src/table.cpp
+++ src/table.cpp
@@ -13,12 +13,12 @@ Table::Table(float centerX, float centerZ, float width, float length, float hole
       length(length) {
     
     // Calculate the boundaries of the table
-    float halfWidth = width / 2.0f;
-    float halfLength = length / 2.0f;
-    float left = centerX - halfWidth;
-    float right = centerX + halfWidth;
-    float top = centerZ - halfLength;
-    float bottom = centerZ + halfLength;
+    const float halfWidth = width / 2.0f;
+    const float halfLength = length / 2.0f;
+    const float left = centerX - halfWidth;
+    const float right = centerX + halfWidth;
+    const float top = centerZ - halfLength;
+    const float bottom = centerZ + halfLength;
 
     // Define the 6 hole positions
     // Corner holes
@@ -27,9 +27,11 @@ Table::Table(float centerX, float centerZ, float width, float length, float hole
     holePositions.push_back(glm::vec2(left, bottom));
     holePositions.push_back(glm::vec2(right, bottom));
 
+    const float middle_hole_offset = holeRadius * 0.40f;
+
     // Middle holes
-    holePositions.push_back(glm::vec2(left, centerZ));
-    holePositions.push_back(glm::vec2(right, centerZ));
+    holePositions.push_back(glm::vec2(left - middle_hole_offset, centerZ));
+    holePositions.push_back(glm::vec2(right + middle_hole_offset, centerZ));
 }
 
 float Table::getCenterX() const {
